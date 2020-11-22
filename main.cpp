edfbcee56de6535cd45dbf011511279f4bd57f0f
quang <nguyenquang1696@gmail.com>
2020-11-22 22:39:49 +0700

    update

--- main.cpp
+++ main.cpp
@@ -3,6 +3,62 @@
 #include "libssh/libsshpp.hpp"
 #include <iostream>
 
+int show_remote_processes(ssh::Session& s)
+{
+    ssh::Channel channel(s);
+    int rc;
+    char buffer[256];
+    int nbytes;
+
+    channel.openSession();
+
+    channel.requestExec("ps aux");
+
+    nbytes = channel.read(buffer,sizeof(buffer),-1);
+    while(nbytes > 0)
+    {
+        if(write(1,buffer,nbytes) != (unsigned int)nbytes)
+        {
+            channel.close();
+            return false;
+        }
+        nbytes = channel.read(buffer,sizeof(buffer),-1);
+    }
+    if(nbytes < 0)
+    {
+        channel.close();
+    }
+    channel.sendEof();
+    channel.close();
+    return true;
+}
+int direct_tcp(ssh::Session& s)
+{
+    int rc;
+    char *http_get = "GET / HTTP/1.1\nHost: www.google.com\n\n";
+    int nbytes, nwritten;
+    char buffer[256];
+    ssh::Channel channel(s);
+    rc = channel.openForward("www.google.com",443,"localhost",5555);
+    nbytes =strlen(http_get);
+    nwritten = channel.write(http_get,nbytes);
+    if(nbytes != nwritten)
+    {
+        return false;
+    }
+    nbytes = channel.read(buffer,sizeof(buffer),-1);
+    while(nbytes > 0)
+    {
+        if(write(1,buffer,nbytes) != (unsigned int)nbytes)
+        {
+            channel.close();
+            return false;
+        }
+        nbytes = channel.read(buffer,sizeof(buffer),-1);
+    }
+
+    return true;
+}
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -22,7 +78,8 @@ int main(int argc, char *argv[])
     qDebug() << "auth --> " << auth;
     if(auth == SSH_AUTH_SUCCESS)
     {
-
+        direct_tcp(s);
+//        show_remote_processes(s);
     }
 
     return a.exec();
